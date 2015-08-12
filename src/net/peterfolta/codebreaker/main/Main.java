32bc62e05dc94fbe4c165adbe885dbf740c7b0c3
Peter Folta <mail@peterfolta.net>
2015-08-12 13:56:07 +0100

    Added Copyright StartUp Message

--- src/net/peterfolta/codebreaker/main/Main.java
+++ src/net/peterfolta/codebreaker/main/Main.java
@@ -8,7 +8,7 @@
  * 
  * File:				Main.java
  * Created:				2015/8/6
- * Last modified:		2015/8/10
+ * Last modified:		2015/8/12
  * Author:				Peter Folta <mail@peterfolta.net>
  * 
  * This program is free software: you can redistribute it and/or modify
@@ -38,10 +38,33 @@ public class Main {
 	private static GUIController guiController;
 	
 	public static void main(String[] args) {
+		printStartUpNotice();
+		
 		languageController = new LanguageController();
 		
 		guiController = new GUIController();
 		guiController.showMainWindow();
+	}
+	
+	private static void printStartUpNotice() {
+		System.out.println(
+			Data.APP_NAME + " - " + Data.APP_DESCRIPTION + "\n" +
+			"Version " + Data.APP_VERSION + "\n" +
+			"Copyright (C) " + Data.APP_COPYRIGHT_YEAR + " " + Data.APP_COPYRIGHT_HOLDER + ". All rights reserved.\n" +
+			"\n" +
+			"This program is free software: you can redistribute it and/or modify\n" +
+			"it under the terms of the GNU General Public License as published by\n" +
+			"the Free Software Foundation, either version 3 of the License, or\n" +
+			"(at your option) any later version.\n" +
+			"\n" +
+			"This program is distributed in the hope that it will be useful,\n" +
+			"but WITHOUT ANY WARRANTY; without even the implied warranty of\n" +
+			"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" +
+			"GNU General Public License for more details.\n" +
+			"\n" +
+			"You should have received a copy of the GNU General Public License\n" +
+			"along with this program.  If not, see <http://www.gnu.org/licenses/>."
+		);
 	}
 	
 	public static Language getLanguage() {
